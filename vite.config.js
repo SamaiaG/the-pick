c30196c2bbb0c96326ec70395a0c4276853ead0c
Samaia Gahramanov <samaiag96@gmail.com>
2024-09-12 20:42:13 +0200

    some config updates

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   base: '/the-pick/',
-  resolve: {
+   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  define: {
-    'process.env': {
-      VITE_API_BASE_URL: process.env.VITE_API_BASE_URL
-    }
-  }
+  } 
 })
